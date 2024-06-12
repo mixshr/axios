ffac624cdb75c9a5a68c8cf8a7e11744736cd445
mihailsarenko <mixail.sharenko@gmail.com>
2024-06-12 13:00:09 +0500

    commit

---
+++ src/examples/1-first-request.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import axios from "axios";
+import {wait} from "@testing-library/user-event/dist/utils";
+// limit, if 429 wait for 15 min and try again
+const url = 'https://www.course-api.com/react-store-products';
+
+const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>first request</h2>;
+};
+export default FirstRequest;
